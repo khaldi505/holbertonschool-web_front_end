045d0110b71b81fcbd76af3723a5a99b700f539c
khaldi505 <habetlbrika69@gmail.com>
2021-06-21 17:03:11 -0700

    3-classrooms.js

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,17 @@
+function createClassRoom(numbersOfStudents) {
+    function studentSeat(seat) {
+            return function() {
+                return seat;
+            };
+    };
+    let students = []
+    for (let i = 0; i < numbersOfStudents; i++){
+        students[i] = studentSeat(i + 1);
+    };
+    return students;
+}
+
+let classRoom = createClassRoom(10);
+console.log(classRoom[0]());
+console.log(classRoom[3]());
+console.log(classRoom[9]());
