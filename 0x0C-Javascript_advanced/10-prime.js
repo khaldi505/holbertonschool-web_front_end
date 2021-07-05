3b4eb3bbda37101097cc46164fd365b340406696
khaldi505 <habetlbrika69@gmail.com>
2021-07-05 04:13:58 -0700

    10-prime.js

--- 0x0C-Javascript_advanced/10-prime.js
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -21,4 +21,4 @@ function countPrimeNumbers(){
 const t0 = performance.now()
 for(let i=0; i<= 100;i++){countPrimeNumbers()}
 const t1 = performance.now()
-console.log("Execution time of printing countPrimeNumbers was " + (t1 - t0) + " milliseconds.")
+console.log("Execution time of calculating prime numbers 100 times was " + (t1 - t0) + " milliseconds.")
